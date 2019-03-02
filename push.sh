36d9503ba400f94e129e7c8303b68d3291fe0b5a
ogbinar <myk.ogbinar@yahoo.com.ph>
2019-03-03 03:30:50 +0800

    scripted update.

--- push.sh
+++ push.sh
@@ -1,6 +1,12 @@
+
+cd /home/pi/ogbinar.github.io/
 /usr/bin/python /home/pi/ogbinar.github.io/update_link.py
 /usr/bin/python /home/pi/ogbinar.github.io/tag_generator.py
 /usr/local/bin/jekyll build /home/pi/ogbinar.github.io
+
+
+/usr/bin/git config --global user.email "myk.ogbinar@yahoo.com.ph"
+/usr/bin/git config --global user.name "ogbinar"
 /usr/bin/git  -C /home/pi/ogbinar.github.io/  add /home/pi/ogbinar.github.io/*
 /usr/bin/git  -C /home/pi/ogbinar.github.io/  commit -m "scripted update."
 /usr/bin/git -C /home/pi/ogbinar.github.io/   push https://ogbinar:ogbinar123@github.com/ogbinar/ogbinar.github.io.git 
