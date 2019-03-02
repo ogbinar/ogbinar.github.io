a569981342aea0a95cf0c96e365e71ca331f7dd4
ogbinar <myk.ogbinar@yahoo.com.ph>
2019-03-03 03:20:51 +0800

    scripted update.

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 /usr/bin/python /home/pi/ogbinar.github.io/update_link.py
 /usr/bin/python /home/pi/ogbinar.github.io/tag_generator.py
 /usr/local/bin/jekyll build /home/pi/ogbinar.github.io
-/usr/bin/git add /home/pi/ogbinar.github.io/*
-/usr/bin/git commit -m "scripted update."
+/usr/bin/git  -C /home/pi/ogbinar.github.io/  add /home/pi/ogbinar.github.io/*
+/usr/bin/git  -C /home/pi/ogbinar.github.io/  commit -m "scripted update."
 /usr/bin/git -C /home/pi/ogbinar.github.io/   push https://ogbinar:ogbinar123@github.com/ogbinar/ogbinar.github.io.git 
