c937be8133bc4c678dffb743edc0fb42bc5bfa61
ogbinar <myk.ogbinar@yahoo.com.ph>
2019-03-03 03:19:05 +0800

    scripted update.

--- push.sh
+++ push.sh
@@ -3,4 +3,4 @@
 /usr/local/bin/jekyll build /home/pi/ogbinar.github.io
 /usr/bin/git add /home/pi/ogbinar.github.io/*
 /usr/bin/git commit -m "scripted update."
-/usr/bin/git -C /home/pi/ogbinar.github.io/   push  origin master  
+/usr/bin/git -C /home/pi/ogbinar.github.io/   push https://ogbinar:ogbinar123@github.com/ogbinar/ogbinar.github.io.git 
